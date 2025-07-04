9022eb333c113e147b970f5d963d34d2fb61063b
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-04 17:43:12 +0200

    [UPD]_(problem setup and declare variables")

---
+++ js-dadi/script.js
@@ -0,0 +1,12 @@
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+//Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+//fisso 2 variabili che facciano da contenitore ai numeri generati per utente e pc
+
+let userPoints = 0;
+let pcPoints = 0
+
+//faccio generare 2 numeri casuali da 1 a 6 e li assegno a user e pc
+
+//eseguo il controllo e dichiaro il vincitore
+
