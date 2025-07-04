d1242b4c4b975d68890e2c1f61c6f858216495da
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-04 17:06:41 +0200

    [UPD]_(problem setup and get user's mail)

---
+++ js-mail/script.js
@@ -0,0 +1,13 @@
+// Crea una lista di email di invitati ad una festa.
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo
+
+
+//faccio inserire l'email all'utente e lo salvo in una variabile mail
+const mail = prompt("inserisci la tua mail")
+console.log(mail)
+
+//definisco la lista di chi può accedere in un array validMail
+
+//utilizzo un ciclo for per determinare se l'utente è presente nella lista
